21eb9e64c4aba192958cb7b1f89e57113d012698
vectorsize <vectorsize@gmail.com>
2015-02-01 20:38:46 +0100

    backup reference of old info object; to be set per extractor object

---
+++ src/feature-info.js
@@ -0,0 +1,65 @@
+module.exportts = {
+
+  "buffer": {
+    "type": "array"
+  },
+  "rms": {
+    "type": "number"
+  },
+  "energy": {
+    "type": "number"
+  },
+  "zcr": {
+    "type": "number"
+  },
+  "complexSpectrum": {
+    "type": "multipleArrays",
+    "arrayNames": {
+      "1": "real",
+      "2": "imag"
+    }
+  },
+  "amplitudeSpectrum": {
+    "type": "array"
+  },
+  "powerSpectrum": {
+    "type": "array"
+  },
+  "spectralCentroid": {
+    "type": "number"
+  },
+  "spectralFlatness": {
+    "type": "number"
+  },
+  "spectralSlope": {
+    "type": "number"
+  },
+  "spectralRolloff": {
+    "type": "number"
+  },
+  "spectralSpread": {
+    "type": "number"
+  },
+  "spectralSkewness": {
+    "type": "number"
+  },
+  "spectralKurtosis": {
+    "type": "number"
+  },
+  "loudness": {
+    "type": "multipleArrays",
+    "arrayNames": {
+      "1": "total",
+      "2": "specific"
+    }
+  },
+  "perceptualSpread": {
+    "type": "number"
+  },
+  "perceptualSharpness": {
+    "type": "number"
+  },
+  "mfcc": {
+    "type": "array"
+  }
+};
